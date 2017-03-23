dfd4976f655b33516dbb90e60fce2aabe8c8ec94
Daniel Clark <danieljclark87@gmail.com>
2017-03-22 23:29:19 -0400

    Added second activity.

--- app/src/main/java/io/github/dclark87/tuneup/MainActivity.java
+++ app/src/main/java/io/github/dclark87/tuneup/MainActivity.java
@@ -1,13 +1,35 @@
 package io.github.dclark87.tuneup;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String EXTRA_MESSAGE = "io.github.dclark87.tuneup.MESSAGE";
 
+    /**
+     *
+     * @param savedInstanceState
+     */
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    /**
+     *
+     * @param view
+     */
+    public void sendMessage(View view) {
+        Intent intent = new Intent(this, DisplayMessageActivity.class);
+        EditText editText = (EditText) findViewById(R.id.editText);
+        String message = editText.getText().toString();
+        // Store key-value pair in intent for message passing.
+        intent.putExtra(EXTRA_MESSAGE, message);
+        // startActivity is inherited from the base Activity class.
+        startActivity(intent);
     }
 }
